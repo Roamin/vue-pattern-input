652fdf2dc3bf9aec5e2faec1c4e47917856b0b82
color vivid <color vivid>
2017-05-25 10:19:34 +0800

    fix component can't response value prop

--- src/component/vue-pattern-input.vue
+++ src/component/vue-pattern-input.vue
@@ -1,70 +1,76 @@
 <template>
-  <input :value="value"
-         @input="updateValue($event.target.value)"
-         v-model="val"
-         ref="input">
+    <input :value="value"
+           @input="updateValue($event.target.value)"
+           v-model="val"
+           ref="input">
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
-    mounted () {
-      this.updateValue(this.value);
-    },
-    name: 'vue-pattern-input',
-    props: {
-      value: {
-        required: true,
-        type: [Number, String]
-      },
-      // Using for: RegExp(pattern[, flags])
-      pattern: {
-        type: String,
-        default: ''
-      },
-      // Using for: RegExp(pattern[, flags])
-      flags: {
-        type: String,
-        default: 'g'
-      },
-      // Using for: String.prototype.replace(regexp, replacement)
-      replacement: {
-        type: String,
-        default: ''
-      }
-    },
-    data () {
-      return {
-          val: ''
-      };
-    },
-    computed: {
-      // Using for: String.prototype.replace(regexp, newSubstr)
-      regExp () {
-        let regExp = '';
+    export default {
+        mounted() {
+            this.updateValue(this.value);
+        },
+        name: 'vue-pattern-input',
+        props: {
+            value: {
+                required: true,
+                type: [Number, String]
+            },
+            // Using for: RegExp(pattern[, flags])
+            pattern: {
+                type: String,
+                default: ''
+            },
+            // Using for: RegExp(pattern[, flags])
+            flags: {
+                type: String,
+                default: 'g'
+            },
+            // Using for: String.prototype.replace(regexp, replacement)
+            replacement: {
+                type: String,
+                default: ''
+            }
+        },
+        data() {
+            return {
+                val: ''
+            };
+        },
+        computed: {
+            // Using for: String.prototype.replace(regexp, newSubstr)
+            regExp() {
+                let regExp = '';
 
-        if (this.pattern !== '') {
-          regExp = new RegExp(this.pattern, this.flags);
+                if (this.pattern !== '') {
+                    regExp = new RegExp(this.pattern, this.flags);
+                }
+
+                return regExp;
+            }
+        },
+        methods: {
+            // format the value of input
+            formatValue(val) {
+                const formattedValue = val.toString().replace(this.regExp, this.replacement);
+
+                return formattedValue;
+            },
+            // update the value of input
+            updateValue(val) {
+                const formattedValue = this.formatValue(val);
+
+                this.val = formattedValue;
+                this.$emit('input', formattedValue);
+            }
+        },
+        watch: {
+            // watch value prop
+            value(val) {
+                this.updateValue(val);
+            }
         }
-
-        return regExp;
-      }
-    },
-    methods: {
-      // format the value of input
-      formatValue (val) {
-        let formattedValue = val.toString().replace(this.regExp, this.replacement);
-
-        return formattedValue;
-      },
-      // update the value of input
-      updateValue (val) {
-        let formattedValue = this.formatValue(val);
-
-        this.val = formattedValue;
-        this.$emit('input', formattedValue);
-      }
     }
-  }
 </script>
 
 <style>
--- src/js/app/demo/component/Demo.vue
+++ src/js/app/demo/component/Demo.vue
@@ -1,146 +1,160 @@
 <template>
-  <div class="container-fluid" id="demo">
-    <div class="col-md-6 col-md-offset-3 mt20">
+    <div class="container-fluid" id="demo">
+        <div class="col-md-6 col-md-offset-3 mt20">
 
-      <div class="form-horizontal">
-        <div class="form-group">
-          <div class="col-sm-12">
-            <div class="alert alert-info"><i class="glyphicon glyphicon-cog mr5"></i>Click the flow buttons to custom quickly</div>
-          </div>
-        </div>
+            <div class="form-horizontal">
+                <div class="form-group">
+                    <div class="col-sm-12">
+                        <div class="alert alert-info"><i class="glyphicon glyphicon-cog mr5"></i>Click the flow buttons to custom quickly
+                        </div>
+                    </div>
+                </div>
 
-        <div class="form-group">
-          <div class="col-sm-12 setting-group">
-            <button type="button" class="btn btn-sm btn-default" :class="{ 'btn-success': setting.selected }"
-                    v-for="(setting, index) in quickSettings"
-                    @click="set(setting, index)">{{ setting.name }}</button>
-          </div>
-        </div>
+                <div class="form-group">
+                    <div class="col-sm-12 setting-group">
+                        <button type="button" class="btn btn-sm btn-default"
+                                :class="{ 'btn-success': setting.selected }"
+                                v-for="(setting, index) in quickSettings"
+                                @click="set(setting, index)">{{ setting.name }}
+                        </button>
+                    </div>
+                </div>
 
-        <hr>
+                <hr>
 
-        <div class="form-group">
-          <div class="col-sm-12">
-            <div class="alert alert-info"><i class="glyphicon glyphicon-pencil mr5"></i>Custom Settings</div>
-          </div>
-        </div>
+                <div class="form-group">
+                    <div class="col-sm-12">
+                        <div class="alert alert-info"><i class="glyphicon glyphicon-pencil mr5"></i>Custom Settings
+                        </div>
+                    </div>
+                </div>
 
-        <div class="form-group">
-          <div class="col-sm-6">
-            <div class="input-group">
-              <span class="input-group-addon">RegExp Pattern</span>
-              <input type="text" class="form-control"
-                     v-model="setting.pattern">
+                <div class="form-group">
+                    <div class="col-sm-6">
+                        <div class="input-group">
+                            <span class="input-group-addon">RegExp Pattern</span>
+                            <input type="text" class="form-control"
+                                   v-model="setting.pattern">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-sm-6">
+                        <div class="input-group">
+                            <span class="input-group-addon">RegExp Flags</span>
+                            <input type="text" class="form-control"
+                                   v-model="setting.flags">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-sm-6">
+                        <div class="input-group">
+                            <span class="input-group-addon">Replacement</span>
+                            <input type="text" class="form-control"
+                                   :disabled="setting.pattern === ''"
+                                   v-model="setting.replacement">
+                        </div>
+                    </div>
+                </div>
+
+                <hr>
+
+                <div class="form-group">
+                    <div class="col-sm-12">
+                        <div class="alert alert-success"><i class="glyphicon glyphicon-arrow-down mr5"></i>Come on, try your custom input now !
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-sm-12">
+                        <pattern-input class="form-control" maxlength="10" placeholder="maxlength 10"
+                                       :pattern="setting.pattern"
+                                       :flags="setting.flags"
+                                       :replacement="setting.replacement"
+                                       v-model="setting.val"></pattern-input>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-sm-12">
+                        <button type="button" class="btn btn-danger" @click="clearInput">clear input</button>
+                    </div>
+                </div>
+
             </div>
-          </div>
+
+
         </div>
-
-        <div class="form-group">
-          <div class="col-sm-6">
-            <div class="input-group">
-              <span class="input-group-addon">RegExp Flags</span>
-              <input type="text" class="form-control"
-                     v-model="setting.flags">
-            </div>
-          </div>
-        </div>
-
-        <div class="form-group">
-          <div class="col-sm-6">
-            <div class="input-group">
-              <span class="input-group-addon">Replacement</span>
-              <input type="text" class="form-control"
-                     :disabled="setting.pattern === ''"
-                     v-model="setting.replacement">
-            </div>
-          </div>
-        </div>
-
-        <hr>
-
-        <div class="form-group">
-          <div class="col-sm-12">
-            <div class="alert alert-success"><i class="glyphicon glyphicon-arrow-down mr5"></i>Come on, try your custom input now !</div>
-          </div>
-        </div>
-
-        <div class="form-group">
-          <div class="col-sm-12">
-            <pattern-input class="form-control" maxlength="10" placeholder="maxlength 10"
-                           :pattern="setting.pattern"
-                           :flags="setting.flags"
-                           :replacement="setting.replacement"
-                           v-model="setting.val"></pattern-input>
-          </div>
-        </div>
-
-      </div>
-
-
     </div>
-  </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import PatternInput from '../../../../component/vue-pattern-input.vue';
+    import PatternInput from '../../../../component/vue-pattern-input.vue';
 
-  export default {
-    components: {
-      PatternInput
-    },
-    data () {
-      return {
-        setting: {
-          pattern: '^[0\\D]*|\\D*',
-          flags: 'g',
-          replacement: '',
-          val: ''
+    export default {
+        components: {
+            PatternInput
         },
-        quickSettings: [
-          {
-            name: 'Noting',
-            pattern: '',
-            flags: '',
-            replacement: '',
-            selected: false
-          },
-          {
-            name: 'Positive Integer',
-            pattern: '^[0\\D]*|\\D*',
-            flags: 'g',
-            replacement: '',
-            selected: true
-          }
-        ]
-      };
-    },
-    methods: {
-      set (oSetting, nIndex) {
-        this.setting.pattern = oSetting.pattern;
-        this.setting.flags = oSetting.flags;
-        this.setting.replacement = oSetting.replacement;
+        data () {
+            return {
+                setting: {
+                    pattern: '^[0\\D]*|\\D*',
+                    flags: 'g',
+                    replacement: '',
+                    val: ''
+                },
+                quickSettings: [
+                    {
+                        name: 'Noting',
+                        pattern: '',
+                        flags: '',
+                        replacement: '',
+                        selected: false
+                    },
+                    {
+                        name: 'Positive Integer',
+                        pattern: '^[0\\D]*|\\D*',
+                        flags: 'g',
+                        replacement: '',
+                        selected: true
+                    }
+                ]
+            };
+        },
+        methods: {
+            set (oSetting, nIndex) {
+                this.setting.pattern = oSetting.pattern;
+                this.setting.flags = oSetting.flags;
+                this.setting.replacement = oSetting.replacement;
 
-        this.setSelectedStatus(nIndex);
-      },
-      setSelectedStatus (nIndex) {
-        this.quickSettings.map((setting, index) => {
-            if (nIndex === index) {
-              setting.selected = true;
-            } else {
-              setting.selected = false;
+                this.setSelectedStatus(nIndex);
+            },
+            setSelectedStatus (nIndex) {
+                this.quickSettings.map((setting, index) => {
+                    if (nIndex === index) {
+                        setting.selected = true;
+                    } else {
+                        setting.selected = false;
+                    }
+
+                    return true;
+                });
+            },
+            clearInput() {
+                this.setting.val = '';
             }
-
-            return true;
-        });
-      }
+        }
     }
-  }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-  .setting-group {
-    .btn+ .btn {
-      margin-left: 5px;
+    .setting-group {
+        .btn + .btn {
+            margin-left: 5px;
+        }
     }
-  }
 </style>
